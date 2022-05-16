68aa91f5674fe18fb84be3bcc38efd57d29810ba
Fakepng <krit0kasemtewin@gmail.com>
2022-05-16 15:46:39 +0700

    Add Helmet custom header

--- src/PageNotFound.jsx
+++ src/PageNotFound.jsx
@@ -1,31 +1,45 @@
+import { Helmet } from "react-helmet";
+
 const PageNotFound = () => {
 	return (
-		<div
-			id='wrapper'
-			style={{
-				backgroundColor: "#FFE4EA",
-				minHeight: "calc(100vh - 54px)",
-			}}
-			className='d-grid h-100'
-		>
-			<img
-				src='https://i.imgur.com/lKJiT77.png'
-				alt='A Dog Ate this Page'
-				style={{ maxWidth: "80%", margin: "auto" }}
-			/>
+		<>
+			<Helmet>
+				<title>Page Not Found</title>
+				<meta property='og:title' content='Page Not Found' />
+				<meta property='og:type' content='website' />
+				<meta property='og:url' content='https://sp617.fakepng.com' />
+				<meta
+					property='og:image'
+					content='https://sp617.fakepng.com/SP512.png'
+				/>
+			</Helmet>
 			<div
-				id='info'
-				style={{ maxWidth: "80%", margin: "auto", textAlign: "center" }}
+				id='wrapper'
+				style={{
+					backgroundColor: "#FFE4EA",
+					minHeight: "calc(100vh - 54px)",
+				}}
+				className='d-grid h-100'
 			>
-				<h3>A Dog Ate this Page</h3>
-				<p>
-					Your dog is cute but honestly a menace. Where are my shoes? Where is
-					my graduation certificate? Where is the chocolate cake I baked for my
-					Aunt’s birthday? And why did you take your dog to the vet on that same
-					Thursday?!
-				</p>
+				<img
+					src='https://i.imgur.com/lKJiT77.png'
+					alt='A Dog Ate this Page'
+					style={{ maxWidth: "80%", margin: "auto" }}
+				/>
+				<div
+					id='info'
+					style={{ maxWidth: "80%", margin: "auto", textAlign: "center" }}
+				>
+					<h3>A Dog Ate this Page</h3>
+					<p>
+						Your dog is cute but honestly a menace. Where are my shoes? Where is
+						my graduation certificate? Where is the chocolate cake I baked for
+						my Aunt’s birthday? And why did you take your dog to the vet on that
+						same Thursday?!
+					</p>
+				</div>
 			</div>
-		</div>
+		</>
 	);
 };
 
--- src/PageUndercon.jsx
+++ src/PageUndercon.jsx
@@ -1,37 +1,51 @@
+import { Helmet } from "react-helmet";
+
 const PageUndercon = () => {
 	return (
-		<div
-			id='wrapper'
-			style={{
-				backgroundColor: "#83D9C8",
-				minHeight: "calc(100vh - 54px)",
-			}}
-			className='d-grid h-100'
-		>
-			<img
-				src='https://i.imgur.com/qIufhof.png'
-				alt='A Dog Ate this Page'
-				style={{ maxWidth: "80%", margin: "auto" }}
-			/>
+		<>
+			<Helmet>
+				<title>Page Under Construction</title>
+				<meta property='og:title' content='Page Under Construction' />
+				<meta property='og:type' content='website' />
+				<meta property='og:url' content='https://sp617.fakepng.com' />
+				<meta
+					property='og:image'
+					content='https://sp617.fakepng.com/SP512.png'
+				/>
+			</Helmet>
 			<div
-				id='info'
+				id='wrapper'
 				style={{
-					maxWidth: "80%",
-					margin: "auto",
-					textAlign: "center",
-					color: "#965418",
+					backgroundColor: "#83D9C8",
+					minHeight: "calc(100vh - 54px)",
 				}}
+				className='d-grid h-100'
 			>
-				<h3>Caution! This Page is Cordoned Off</h3>
-				<p>
-					The earthquake was not good to the bike lane on your way to work. A
-					large gap in the pavement (too big to be called a pothole) had
-					swallowed three oblivious bikers whole. So the city had put up two
-					pylons and yellow caution tape. Pretty frustrating for you given your
-					propensity to do 360 jumps over the gap.
-				</p>
+				<img
+					src='https://i.imgur.com/qIufhof.png'
+					alt='A Dog Ate this Page'
+					style={{ maxWidth: "80%", margin: "auto" }}
+				/>
+				<div
+					id='info'
+					style={{
+						maxWidth: "80%",
+						margin: "auto",
+						textAlign: "center",
+						color: "#965418",
+					}}
+				>
+					<h3>Caution! This Page is Cordoned Off</h3>
+					<p>
+						The earthquake was not good to the bike lane on your way to work. A
+						large gap in the pavement (too big to be called a pothole) had
+						swallowed three oblivious bikers whole. So the city had put up two
+						pylons and yellow caution tape. Pretty frustrating for you given
+						your propensity to do 360 jumps over the gap.
+					</p>
+				</div>
 			</div>
-		</div>
+		</>
 	);
 };
 
