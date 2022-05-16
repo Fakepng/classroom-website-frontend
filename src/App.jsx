0191331cd244278456fb16d5f5789a62f9090c49
Fakepng <krit0kasemtewin@gmail.com>
2022-05-16 14:10:33 +0700

    Use Protected route
    - Create: Protected route
    - Create PageNotFound
    - Create PageUndercon

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { isExpired, decodeToken } from "react-jwt";
+import Protected from "./Protected";
 import Header from "./components/Header";
 import AdminHeader from "./components/AdminHeader";
 import EditHomework from "./pages/EditHomework";
@@ -7,6 +8,8 @@ import Homework from "./pages/Homework";
 import Login from "./pages/Login";
 import NewUser from "./pages/NewUser";
 import Schedule from "./pages/Schedule";
+import PageNotFound from "./PageNotFound";
+import PageUndercon from "./PageUndercon";
 
 function App() {
 	const token = localStorage.getItem("accessToken");
@@ -14,27 +17,33 @@ function App() {
 	const isTokenExpired = isExpired(token);
 	const tokenOwner = decodeToken(token)?.username;
 
-	if (token && !isTokenExpired && tokenOwner === user) {
-		return (
-			<BrowserRouter>
-				<AdminHeader />
-				<Routes>
-					<Route exact path='/' element={<Schedule />} />
-					<Route path='/homework' element={<Homework />} />
-					<Route path='/login' element={<Login />} />
-					<Route path='/new-user' element={<NewUser />} />
-					<Route path='/edit-homework' element={<EditHomework />} />
-				</Routes>
-			</BrowserRouter>
-		);
-	}
+	const isUser = token && !isTokenExpired && tokenOwner === user;
+
 	return (
 		<BrowserRouter>
-			<Header />
+			{isUser ? <AdminHeader /> : <Header />}
 			<Routes>
 				<Route exact path='/' element={<Schedule />} />
 				<Route path='/homework' element={<Homework />} />
 				<Route path='/login' element={<Login />} />
+				<Route path='/students' element={<PageUndercon />} />
+				<Route
+					path='/new-user'
+					element={
+						<Protected isUser={isUser}>
+							<NewUser />
+						</Protected>
+					}
+				/>
+				<Route
+					path='/edit-homework'
+					element={
+						<Protected isUser={isUser}>
+							<EditHomework />
+						</Protected>
+					}
+				/>
+				<Route path='*' element={<PageNotFound />} />
 			</Routes>
 		</BrowserRouter>
 	);
