da3eb0c70f03f9e35e4989b4926e228b6349c504
Fakepng <krit0kasemtewin@gmail.com>
2022-05-21 22:17:25 +0700

    Add: Click on image to open modal

--- src/pages/Students/index.jsx
+++ src/pages/Students/index.jsx
@@ -1,12 +1,19 @@
+import { useState } from "react";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
 import ButtonGroup from "react-bootstrap/ButtonGroup";
 import classnames from "classnames";
 import StudentsJSON from "../../config/Students.json";
 import "./Students.css";
 
 const Students = () => {
-	const students = StudentsJSON.map((student) => {
+	const [modal, setModal] = useState({ open: false, index: -1 });
+
+	const handleClose = () => setModal({ open: false, index: -1 });
+	const handleOpen = (index) => setModal({ open: true, index });
+
+	const students = StudentsJSON.map((student, index) => {
 		const socials = student.social.map((social) => {
 			const socialColor = classnames({
 				"#4267B2": social.platform === "FB",
@@ -36,6 +43,7 @@ const Students = () => {
 				style={{ width: "18rem", margin: "auto", height: "28rem" }}
 				key={student.id}
 				className='cards'
+				onClick={() => handleOpen(index)}
 			>
 				<Card.Img variant='top' src={student.img} key={student.no} />
 				<Card.Body>
@@ -66,18 +74,43 @@ const Students = () => {
 	});
 
 	return (
-		<div
-			style={{
-				display: "grid",
-				gridTemplateColumns: "repeat(auto-fit, minmax(18rem, 1fr))",
-				gap: "2rem",
-				margin: "2rem auto",
-				width: "80%",
-				maxWidth: "1100px",
-			}}
-		>
-			{students}
-		</div>
+		<>
+			<div
+				style={{
+					display: "grid",
+					gridTemplateColumns: "repeat(auto-fit, minmax(18rem, 1fr))",
+					gap: "2rem",
+					margin: "2rem auto",
+					width: "80%",
+					maxWidth: "1100px",
+				}}
+			>
+				{students}
+			</div>
+			{modal.open ? (
+				<Modal show={modal.open} onHide={handleClose}>
+					<Modal.Header closeButton>
+						<Modal.Title>{StudentsJSON[modal.index].name}</Modal.Title>
+					</Modal.Header>
+					<Modal.Body>
+						<img
+							src={StudentsJSON[modal.index].img}
+							alt={StudentsJSON[modal.index].nickname}
+							style={{ width: "100%" }}
+						/>
+						{/* {StudentsJSON[modal.index].id} */}
+					</Modal.Body>
+					{/* <Modal.Footer>
+						<Button variant='secondary' onClick={handleClose}>
+							Close
+						</Button>
+						<Button variant='primary' onClick={handleClose}>
+							Save Changes
+						</Button>
+					</Modal.Footer> */}
+				</Modal>
+			) : null}
+		</>
 	);
 };
 
