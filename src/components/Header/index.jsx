766dad08617d2650419a15b5b29445cd530cc7cb
Fakepng <krit0kasemtewin@gmail.com>
2022-06-23 13:22:50 +0700

    Refactor Nav.Link as NavLink

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,40 +1,38 @@
 import { Container, Nav, Navbar } from "react-bootstrap";
-import { LinkContainer } from "react-router-bootstrap";
+import { NavLink } from "react-router-dom";
 
 const Header = () => {
 	return (
 		<Navbar bg='light' expand='lg' sticky='top'>
 			<Container>
-				<LinkContainer to='/'>
-					<Navbar.Brand>
-						<img
-							src='/SP512.png'
-							width='30'
-							height='30'
-							className='d-inline-block align-top'
-							alt='SP logo'
-						/>
-						617
-					</Navbar.Brand>
-				</LinkContainer>
+				<Navbar.Brand to='/' as={NavLink}>
+					<img
+						src='/SP512.png'
+						width='30'
+						height='30'
+						className='d-inline-block align-top'
+						alt='SP logo'
+					/>
+					617
+				</Navbar.Brand>
 				<Navbar.Toggle aria-controls='basic-navbar-nav' />
 				<Navbar.Collapse id='basic-navbar-nav'>
 					<Nav className='me-auto'>
-						<LinkContainer to='/'>
-							<Nav.Link>Home</Nav.Link>
-						</LinkContainer>
-						<LinkContainer to='/homework'>
-							<Nav.Link>Homework</Nav.Link>
-						</LinkContainer>
-						<LinkContainer to='/students'>
-							<Nav.Link>Students</Nav.Link>
-						</LinkContainer>
-						<LinkContainer to='/teachers'>
-							<Nav.Link>Teachers</Nav.Link>
-						</LinkContainer>
-						<LinkContainer to='/login'>
-							<Nav.Link>Login</Nav.Link>
-						</LinkContainer>
+						<Nav.Link to='/' as={NavLink}>
+							Home
+						</Nav.Link>
+						<Nav.Link to='/homework' as={NavLink}>
+							Homework
+						</Nav.Link>
+						<Nav.Link to='/students' as={NavLink}>
+							Students
+						</Nav.Link>
+						<Nav.Link to='/teachers' as={NavLink}>
+							Teachers
+						</Nav.Link>
+						<Nav.Link to='/login' as={NavLink}>
+							Login
+						</Nav.Link>
 					</Nav>
 				</Navbar.Collapse>
 			</Container>
