da8e7b99b53ab3148b7f5859528a223d29cfb166
Fakepng <krit0kasemtewin@gmail.com>
2022-06-24 01:29:32 +0700

    Add: version tracking

--- src/components/AdminHeader/index.jsx
+++ src/components/AdminHeader/index.jsx
@@ -1,56 +1,66 @@
+import { useState, useEffect } from "react";
 import { Container, Nav, Navbar } from "react-bootstrap";
-import { LinkContainer } from "react-router-bootstrap";
+import { NavLink } from "react-router-dom";
+import axios from "axios";
+import Client from "../../config/version";
 
 const AdminNavbar = () => {
+	const [Server, setServer] = useState("");
+
 	const handleLogout = () => {
 		localStorage.removeItem("accessToken");
 		window.location.reload(false);
 		alert("Logout success");
 	};
 
+	useEffect(() => {
+		axios.get(`${process.env.REACT_APP_API_LINK}/version`).then((response) => {
+			setServer(response.data.version);
+		});
+	}, []);
+
 	return (
 		<Navbar bg='light' expand='lg' sticky='top'>
 			<Container>
-				<LinkContainer to='/'>
-					<Navbar.Brand>
-						<img
-							src='/SP512.png'
-							width='30'
-							height='30'
-							className='d-inline-block align-top'
-							alt='SP logo'
-						/>
-						617
-					</Navbar.Brand>
-				</LinkContainer>
+				<Navbar.Brand to='/' as={NavLink}>
+					<img
+						src='/SP512.png'
+						width='30'
+						height='30'
+						className='d-inline-block align-top'
+						alt='SP logo'
+					/>
+					617
+				</Navbar.Brand>
 				<Navbar.Toggle aria-controls='basic-navbar-nav' />
 				<Navbar.Collapse id='basic-navbar-nav'>
 					<Nav className='me-auto'>
-						<LinkContainer to='/'>
-							<Nav.Link>Home</Nav.Link>
-						</LinkContainer>
-						<LinkContainer to='/homework'>
-							<Nav.Link>Homework</Nav.Link>
-						</LinkContainer>
-						<LinkContainer to='/students'>
-							<Nav.Link>Students</Nav.Link>
-						</LinkContainer>
-						<LinkContainer to='/teachers'>
-							<Nav.Link>Teachers</Nav.Link>
-						</LinkContainer>
-						<LinkContainer to='/edit-homework'>
-							<Nav.Link>Edit Homework</Nav.Link>
-						</LinkContainer>
-						<LinkContainer to='/special-schedules'>
-							<Nav.Link>Special Schedules</Nav.Link>
-						</LinkContainer>
-						<LinkContainer to='/new-user'>
-							<Nav.Link>New User</Nav.Link>
-						</LinkContainer>
-						<LinkContainer onClick={handleLogout} to='/'>
-							<Nav.Link>Logout</Nav.Link>
-						</LinkContainer>
+						<Nav.Link to='/' as={NavLink}>
+							Home
+						</Nav.Link>
+						<Nav.Link to='/homework' as={NavLink}>
+							Homework
+						</Nav.Link>
+						<Nav.Link to='/students' as={NavLink}>
+							Students
+						</Nav.Link>
+						<Nav.Link to='/teachers' as={NavLink}>
+							Teachers
+						</Nav.Link>
+						<Nav.Link to='/edit-homework' as={NavLink}>
+							Edit Homework
+						</Nav.Link>
+						<Nav.Link to='/special-schedules' as={NavLink}>
+							Special Schedules
+						</Nav.Link>
+						<Nav.Link to='/new-user' as={NavLink}>
+							New User
+						</Nav.Link>
+						<Nav.Link to='/' as={NavLink} onClick={handleLogout}>
+							Logout
+						</Nav.Link>
 					</Nav>
+					Client: {Client} Server: {Server}
 				</Navbar.Collapse>
 			</Container>
 		</Navbar>
